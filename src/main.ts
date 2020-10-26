6f3b4fcd432aa4a7cefdc6466d8f6578a77927ea
elan rasabi <elan.rasabi@ncr.com>
2020-10-26 18:56:01 -0400

    swagger implemented and reachable at '/api'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder().setTitle('Conduit API').setDescription('description').setVersion('1.0.0').build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
