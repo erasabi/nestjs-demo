515dbe4089525ef2a7cb13973d43504d7b79c58c
elan rasabi <elan.rasabi@ncr.com>
2020-11-25 19:50:08 -0500

    26: PATCH updating task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post, Delete, Param } from '@nestjs/common';
+import { Body, Controller, Get, Post, Delete, Param, Patch } from '@nestjs/common';
 import { TasksService } from "./tasks.service";
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/createTaskDto';
 
 @Controller('tasks')
@@ -25,5 +25,13 @@ export class TasksController {
     @Delete('/:id')
     deleteTask(@Param('id') id: string) {
         return this.tasksService.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus,
+    ): Task {
+        return this.tasksService.updateTaskStatus(id, status);
     }
 }
