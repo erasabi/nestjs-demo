2069ff8698436e843ff00e6a6228a14bc4b22fa0
elan rasabi <elan.rasabi@ncr.com>
2020-11-29 18:10:42 -0500

    32. Error Handling: Deleting Non-Existent Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -56,7 +56,8 @@ export class TasksService {
     }
 
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const task = this.getTaskById(id);
+        this.tasks = this.tasks.filter(t => t.id !== task.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
